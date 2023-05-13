e5e4e3596854f07669c48d1b8ac6cfc0c697f958
Charles Shinaver <cshinaver@plaid.com>
2023-05-13 14:32:18 -0400

    local dev uses sqlite

--- config/dev.exs
+++ config/dev.exs
@@ -2,13 +2,8 @@ import Config
 
 # Configure your database
 config :lexi, Lexi.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "lexi_dev",
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  adapter: Ecto.Adapters.SQLite3,
+  database: "priv/repo/dev.sqlite"
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- lib/lexi/repo.ex
+++ lib/lexi/repo.ex
@@ -1,5 +1,9 @@
 defmodule Lexi.Repo do
   use Ecto.Repo,
     otp_app: :lexi,
-    adapter: Ecto.Adapters.Postgres
+    adapter:
+      if(Mix.env() in [:dev, :test],
+        do: Ecto.Adapters.SQLite3,
+        else: Ecto.Adapters.Postgres
+      )
 end
