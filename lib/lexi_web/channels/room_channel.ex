0d3f54a6fb442595f37dc4e78baac3f4264f6167
Charles Shinaver <cshinaver@plaid.com>
2023-06-25 17:04:03 -0400

    added welcome message

--- lib/lexi_web/channels/room_channel.ex
+++ lib/lexi_web/channels/room_channel.ex
@@ -7,7 +7,14 @@ defmodule LexiWeb.RoomChannel do
   end
 
   def join("room:" <> _private_room_id, _params, socket) do
+    send(self, :after_join)
     {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    message = "Welcome to your private tutor session for Mandarin!"
+    broadcast!(socket, "new_msg", %{body: message})
+    {:noreply, socket}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
